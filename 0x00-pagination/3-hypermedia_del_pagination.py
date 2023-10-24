2d52dbc46fb73d24c131aa398439acf0ae7133c9
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-24 13:06:41 +0100

    feat: cursor based pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+from typing import Any, List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            # truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(
+            self, index: Any,
+            page_size: int = 10) -> Dict[str, Any]:
+        """better pagination mechanism."""
+        idx_data = self.indexed_dataset()
+        found_idx = False
+        out = {}
+        n_count = 0
+
+        # assert isinstance(index, int)
+        assert index < len(idx_data)
+
+        for k, v in idx_data.items():
+            if n_count >= page_size:
+                break
+            if k >= index and found_idx is False:
+                out["index"] = index
+                out["data"] = []
+                out["page_size"] = 0
+                found_idx = True
+            if found_idx is True:
+                out["data"].append(v)
+                out["page_size"] += 1
+                out['next_index'] = k+1
+                n_count += 1
+
+        return out
