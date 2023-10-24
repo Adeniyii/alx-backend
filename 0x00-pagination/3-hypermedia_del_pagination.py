a638f82e47474ae730f5857c4ff16ad4dde2cb30
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-24 14:44:38 +0100

    fix

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,7 +40,7 @@ class Server:
 
     def get_hyper_index(
             self, index: Any,
-            page_size: int = 10) -> Dict[str, Any]:
+            page_size: int = 10) -> Dict:
         """better pagination mechanism."""
         idx_data = self.indexed_dataset()
 
@@ -68,38 +68,3 @@ class Server:
                 n_count += 1
 
         return out
-
-
-if __name__ == "__main__":
-    server = Server()
-
-    server.indexed_dataset()
-
-    try:
-        server.get_hyper_index(300000, 100)
-    except AssertionError:
-        print("AssertionError raised when out of range")
-
-    index = 0
-    page_size = 10
-
-    print("Nb items: {}".format(len(server._Server__indexed_dataset)))
-
-# 1- request first index
-    res = server.get_hyper_index(index, page_size)
-    print(res)
-
-# 2- request next index
-    print(server.get_hyper_index(res.get('next_index'), page_size))
-
-# 3- remove the first index
-    del server._Server__indexed_dataset[res.get('index')]
-    del server._Server__indexed_dataset[res.get('index') + 3]
-    del server._Server__indexed_dataset[res.get('index') + 6]
-    print("Nb items: {}".format(len(server._Server__indexed_dataset)))
-
-# 4- request again the initial index -> the first data retreives is not the same as the first request
-    print(server.get_hyper_index(index, page_size))
-
-# 5- request again initial next index -> same data page as the request 2-
-    print(server.get_hyper_index(res.get('next_index'), page_size))
