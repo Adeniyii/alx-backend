5866b90f88a74c3d986a444df611fe8f09e8e9b2
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-26 05:39:48 +0100

    fix: proper indenting

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,16 +5,37 @@ inherits from `BaseCaching` and is a caching system.
 """
 from cache.base_caching import BaseCaching
 
-class BasicCache(BaseCaching):
-	"""A basic cache datastructure."""
-	def put(self, key, item):
-		"""Inserts an item into the cache using a simple caching algo."""
-		if key is None or item is None:
-			return
-		self.cache_data[key] = item
 
-	def get(self, key):
-		"""Retrieves an item from the cache using a simple caching algo."""
-		if key is None:
-			return
-		return self.cache_data.get(key, None)
+class BasicCache(BaseCaching):
+    """A basic cache datastructure."""
+
+    def put(self, key, item):
+        """Inserts an item into the cache using a simple caching algo."""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves an item from the cache using a simple caching algo."""
+        if key is None:
+            return
+        return self.cache_data.get(key, None)
+
+
+if __name__ == "__main__":
+    my_cache = BasicCache()
+    my_cache.print_cache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    print(my_cache.get("D"))
+    my_cache.print_cache()
+    my_cache.put("D", "School")
+    my_cache.put("E", "Battery")
+    my_cache.put("A", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
