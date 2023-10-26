9a39a5f7fcc943aad677a39f99be8fe6369f9506
Adeniyii <ifedayoadeniyi@gmail.com>
2023-10-26 06:20:31 +0100

    fix

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,41 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 0-basic-cache.py implements a class `BasicCache` that
 inherits from `BaseCaching` and is a caching system.
 """
-# from cache.base_caching import BaseCaching
-
-
-class BaseCaching():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError(
-            "put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError(
-            "get must be implemented in your cache class")
+from cache.base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
@@ -52,22 +20,3 @@ class BasicCache(BaseCaching):
         if key is None:
             return
         return self.cache_data.get(key, None)
-
-
-if __name__ == "__main__":
-    my_cache = BasicCache()
-    my_cache.print_cache()
-    my_cache.put("A", "Hello")
-    my_cache.put("B", "World")
-    my_cache.put("C", "Holberton")
-    my_cache.print_cache()
-    print(my_cache.get("A"))
-    print(my_cache.get("B"))
-    print(my_cache.get("C"))
-    print(my_cache.get("D"))
-    my_cache.print_cache()
-    my_cache.put("D", "School")
-    my_cache.put("E", "Battery")
-    my_cache.put("A", "Street")
-    my_cache.print_cache()
-    print(my_cache.get("A"))
